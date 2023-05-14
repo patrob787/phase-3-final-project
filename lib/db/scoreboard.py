bf7c2babd526f7f061b0fd9deb0efcbcd61520fa
Clifton Gockul <clifga@gmail.com>
2023-05-13 20:45:19 -0400

    day 1

---
+++ lib/db/scoreboard.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, ForeignKey, String
+from sqlalchemy.orm import relationship
+from player import Base
+
+class Scoreboard(Base):
+    __tablename__ = 'scoreboards'
+    id = Column(Integer, primary_key=True)
+    player_id = Column(Integer, ForeignKey('players.id'))
+    player = relationship("Player", back_populates="scoreboards")
+    username = Column(String)
+    score = Column(Integer)
+    remaining_health = Column(Integer)
+
+    def __repr__(self):
+        return f"Scoreboard(id={self.id}, player_id={self.player_id}, score={self.score}, username='{self.player.username}')"
