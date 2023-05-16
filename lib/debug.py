18636142f0df4d003b94b7dafc0446eebdf5fe49
Patrick Roberts <patrob22@gmail.com>
2023-05-16 00:20:01 -0400

    debug.py setup

---
+++ lib/debug.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db.models import Base, Player, Puzzle, Scoreboard, Room
+
+
+
+
+
+
+
+
+
+
+
+
+import ipdb; ipdb.set_trace()
