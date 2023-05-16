0bd6c2fc96d5d93575a4219f02d830d0034d40d9
Patrick Roberts <patrob22@gmail.com>
2023-05-16 00:16:23 -0400

    reformat game-cli

--- lib/game-cli.py
+++ lib/game-cli.py
@@ -7,19 +7,20 @@ new_player = input("Enter your name: ") # get the player's name from input store
 
 print(f'Welcome {new_player}!...................................... begin game')
 
-engine = create_engine("sqlite:///game.db", echo=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+# engine = create_engine("sqlite:///escape_app.db", echo=True)
+# Base.metadata.create_all(engine)
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
 # create a new player and scoreboard and link them together
 player1 = Player(username=new_player, score=400, keys=300)
 room1 = Room(player=player1,username=player1.username, room1=True, room2=False, room3=False, room4=False, room5=False, room6=False, room7=False, room8=False, room9=False)
 score1 = Scoreboard(player=player1, score=player1.score, username=player1.username, games_completed=3)
-session.add(room1)
-session.add(player1)
-session.add(score1)
-session.commit()
+
+# session.add(room1)
+# session.add(player1)
+# session.add(score1)
+# session.commit()
 
 # query the scoreboards and join them with the players
 scoreboards = session.query(Scoreboard).join(Player).all()
