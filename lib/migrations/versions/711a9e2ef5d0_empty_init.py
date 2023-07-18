e0932ac5f9068db3b6f587e0e10799e6b3763a38
Patrick Roberts <patrob22@gmail.com>
2023-07-18 12:45:11 -0400

    DB reinitialized and clean.  Unnecessary instances removed

---
+++ lib/migrations/versions/711a9e2ef5d0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty init
+
+Revision ID: 711a9e2ef5d0
+Revises: 
+Create Date: 2023-07-18 12:43:00.891882
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '711a9e2ef5d0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
